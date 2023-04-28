617acfb3772b062d7efd309a82c66ffce3b13ae2
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-28 22:23:50 +0500

    function
    even or odd find

---
+++ script.js
@@ -0,0 +1,22 @@
+// document;
+// console.log(document);
+// var element=document.getElementById("input1");
+// function sub(params) {
+//     var element=document.getElementById("heading").innerHTML="Kashif";
+//   var element1=document.getElementById("para").innerHTML="javaid";
+//     console.log(element);
+
+
+// }
+
+function sub(params) {
+    var a=+prompt("user enter number")
+    if (a==2%0) {
+        var element=document.getElementById("heading").innerHTML="Nymber is even";
+        console.log(element);
+    }
+    else{
+       console.log("number is odd");
+    }
+}
+
